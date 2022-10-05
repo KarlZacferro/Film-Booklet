aae9ad3a69cf781586a010ec736249e6b6534f37
Karlisson <karlissonferreira42@gmail.com>
2022-10-05 11:18:46 -0300

    Component styling

--- client/src/pages/home/styles.js
+++ client/src/pages/home/styles.js
@@ -7,38 +7,46 @@ export const Container = styled.div`
     margin: 4rem 0;
    }
 
-   ul{
-    list-style: none;
+
+`
+//Criar pasta de componts e separar cada um deles
+   export const MovieList = styled.ul`
+   
+   list-style: none;
     display: grid;
     grid-template-columns: repeat(auto-fit,minmax(200px, 1fr));
     columns-gap: 3rem;
     row-gap: 4rem;
-   }
-
-   li{
-    display: flex;
-    flex-direction: columns;
-    align-items: center;
-   }
-
-   img{
-    width: 180px;
-    border-radius: 1rem;
-    margin-botton: 2rem;
-   }
-
-   span{
-    font-weignt: bold;
-    font-size: 120%;
-   }
+   
     
-   a{
-    transition: all 0.3;
-    }
+   
+   ` 
 
-   a:hover {
-    transform: scale(1.1);
-   }
+   export const Movie = styled.li`
+      display: flex;
+      flex-direction: columns;
+      align-items: center;
+   
+
+      img{
+         width: 180px;
+         border-radius: 1rem;
+         margin-botton: 2rem;
+        }
+     
+        span{
+         font-weignt: bold;
+         font-size: 120%;
+        }
+         
+        a{
+         transition: all 0.3;
+         }
+     
+        a:hover {
+         transform: scale(1.1);
+        }
+   
+   `
 
 
-`

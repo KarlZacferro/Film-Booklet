4d339f8e507054b2d39ad1207645af36b67b35f2
Karlisson <karlissonferreira42@gmail.com>
2022-10-19 17:50:43 -0300

    consume the api

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import  ReactDOM  from 'react-dom';
-import './global.css';
-import Home from './pages/home/home'; 
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Details from './pages/Details';
+import './global.css'
+ 
 
 ReactDOM.render(
-    <React.StrictMode>
-
-    <Home/>
-
-    </React.StrictMode>,
-    document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/details/:id" element={<Details/>}/>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
